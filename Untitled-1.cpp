efc7d4dd82db3f79f838137d18fa7f6171464201
HoverKK <147485931+HoverKK@users.noreply.github.com>
2024-01-10 13:29:31 +0100

    dodanie metod:A+B,A*B,A+int,A*int,A-int

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -9,57 +9,142 @@ private:
     int** data;
 
 public:
-    matrix() : n(0), data(nullptr) {}
-
-    matrix(int size) : n(0), data(nullptr) {
-        alokuj(size);
-    }
-
-    matrix(int size, int* arr) : n(0), data(nullptr) {
-        alokuj(size);
-        memcpy(data[0], arr, n * n * sizeof(int));
-    }
-
-    matrix(const matrix& m) : n(0), data(nullptr) {
-        alokuj(m.n);
-        memcpy(data[0], m.data[0], n * n * sizeof(int));
-    }
-
-    ~matrix() {
+    // Konstruktory
+    matrix(void) : n(0), data(nullptr) {}
+    matrix(int size) : n(0), data(nullptr) { alokuj(size); }
+    matrix(int size, int* arr) : n(0), data(nullptr) { alokuj(size); std::memcpy(data[0], arr, n * n * sizeof(int)); }
+    matrix(const matrix& m) : n(0), data(nullptr) { alokuj(m.n); std::memcpy(data[0], m.data[0], n * n * sizeof(int)); }
+    
+    // Destruktor
+    ~matrix(void) {
         for (int i = 0; i < n; ++i) {
             delete[] data[i];
         }
         delete[] data;
     }
 
+    // Przeciążony operator przypisania
+    matrix& operator=(const matrix& m) {
+        if (this != &m) {
+            alokuj(m.n);
+            std::memcpy(data[0], m.data[0], n * n * sizeof(int));
+        }
+        return *this;
+    }
+
+    // Funkcje dodatkowe
     matrix& alokuj(int size) {
-        if (n > 0) {
+        if (data == nullptr || n != size) {
             for (int i = 0; i < n; ++i) {
                 delete[] data[i];
             }
             delete[] data;
+
+            n = size;
+            data = new int*[n];
+            data[0] = new int[n * n];
+            for (int i = 1; i < n; ++i) {
+                data[i] = data[i - 1] + n;
+            }
         }
-        n = size;
-        data = new int*[n];
-        data[0] = new int[n * n];
-        for (int i = 1; i < n; ++i) {
-            data[i] = data[i - 1] + n;
+        return *this;
+    }
+
+    matrix& wstaw(int row, int col, int value) {
+        if (row >= 0 && row < n && col >= 0 && col < n) {
+            data[row][col] = value;
+        }
+        return *this;
+    }
+
+    int pokaz(int row, int col) {
+        if (row >= 0 && row < n && col >= 0 && col < n) {
+            return data[row][col];
+        }
+        return -1;
+    }
+
+    matrix& dowroc() {
+        int** transposed = new int*[n];
+        for (int i = 0; i < n; ++i) {
+            transposed[i] = new int[n];
+            for (int j = 0; j < n; ++j) {
+                transposed[i][j] = data[j][i];
+            }
+        }
+
+        for (int i = 0; i < n; ++i) {
+            delete[] data[i];
+        }
+        delete[] data;
+
+        data = transposed;
+
+        return *this;
+    }
+
+    matrix& losuj() {
+        srand(time(nullptr));
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                data[i][j] = rand() % 10;
+            }
         }
         return *this;
     }
 
     matrix& losuj(int x) {
-        srand(static_cast<unsigned>(time(nullptr)));
+        srand(time(nullptr));
+        for (int k = 0; k < x; ++k) {
+            int row = rand() % n;
+            int col = rand() % n;
+            data[row][col] = rand() % 10;
+        }
+        return *this;
+    }
+
+    matrix& diagonalna(int* t) {
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
-                data[i][j] = rand() % (x + 1);
+                if (i == j) {
+                    data[i][j] = t[i];
+                } else {
+                    data[i][j] = 0;
+                }
             }
         }
         return *this;
     }
 
-    int pokaz(int x, int y) {
-        return data[x][y];
+    matrix& diagonalna_k(int k, int* t) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i - j == k) {
+                    data[i][j] = t[i];
+                } else {
+                    data[i][j] = 0;
+                }
+            }
+        }
+        return *this;
+    }
+
+    matrix& kolumna(int x, int* t) {
+        if (x >= 0 && x < n) {
+            for (int i = 0; i < n; ++i) {
+                data[i][x] = t[i];
+            }
+        }
+        return *this;
+    }
+
+    matrix& wiersz(int y, int* t) {
+        if (y >= 0 && y < n) {
+            for (int j = 0; j < n; ++j) {
+                data[y][j] = t[j];
+            }
+        }
+        return *this;
     }
 
     matrix& przekatna() {
@@ -98,7 +183,7 @@ public:
         return *this;
     }
 
-    matrix& operator+(matrix& m) {
+   matrix& operator+(matrix& m) {
         matrix* result = new matrix(n);
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
@@ -106,6 +191,65 @@ public:
             }
         }
         return *result;
+    }
+
+    matrix& operator*(matrix& m) {
+        matrix* result = new matrix(n);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int sum = 0;
+                for (int k = 0; k < n; ++k) {
+                    sum += data[i][k] * m.data[k][j];
+                }
+                result->data[i][j] = sum;
+            }
+        }
+        return *result;
+    }
+
+    matrix& operator+(int a) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                data[i][j] += a;
+            }
+        }
+        return *this;
+    }
+
+    matrix& operator*(int a) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                data[i][j] *= a;
+            }
+        }
+        return *this;
+    }
+
+    matrix& operator-(int a) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                data[i][j] -= a;
+            }
+        }
+        return *this;
+    }
+
+    friend matrix operator+(int a, matrix& m) {
+        matrix result = m;
+        result + a;
+        return result;
+    }
+
+    friend matrix operator*(int a, matrix& m) {
+        matrix result = m;
+        result * a;
+        return result;
+    }
+
+    friend matrix operator-(int a, matrix& m) {
+        matrix result = m;
+        result - a;
+        return result;
     }
 };
 
@@ -122,7 +266,7 @@ int main() {
         std::cout << "\n";
     }
 
-    std::cout << "Macierz B:\n";
+    std::cout << "\nMacierz B:\n";
     for (int i = 0; i < 30; ++i) {
         for (int j = 0; j < 30; ++j) {
             std::cout << m2.pokaz(i, j) << " ";
@@ -130,12 +274,47 @@ int main() {
         std::cout << "\n";
     }
 
-    matrix m3 = m1 + m2;
-
-    std::cout << "Macierz A + B:\n";
+    matrix result_add = m1 + m2;
+    std::cout << "\nSuma macierzy A i B:\n";
     for (int i = 0; i < 30; ++i) {
         for (int j = 0; j < 30; ++j) {
-            std::cout << m3.pokaz(i, j) << " ";
+            std::cout << result_add.pokaz(i, j) << " ";
+        }
+        std::cout << "\n";
+    }
+
+    matrix result_multiply = m1 * m2;
+    std::cout << "\nIloczyn macierzy A i B:\n";
+    for (int i = 0; i < 30; ++i) {
+        for (int j = 0; j < 30; ++j) {
+            std::cout << result_multiply.pokaz(i, j) << " ";
+        }
+        std::cout << "\n";
+    }
+
+    matrix result_scalar_multiply = m1 * 5;
+    std::cout << "\nMacierz A pomnożona przez 5:\n";
+    for (int i = 0; i < 30; ++i) {
+        for (int j = 0; j < 30; ++j) {
+            std::cout << result_scalar_multiply.pokaz(i, j) << " ";
+        }
+        std::cout << "\n";
+    }
+
+    matrix result_scalar_add = m1 + 3;
+    std::cout << "\nMacierz A powiększona o 3:\n";
+    for (int i = 0; i < 30; ++i) {
+        for (int j = 0; j < 30; ++j) {
+            std::cout << result_scalar_add.pokaz(i, j) << " ";
+        }
+        std::cout << "\n";
+    }
+
+    matrix result_scalar_subtract = 2 - m1;
+    std::cout << "\n2 odjęte od macierzy A:\n";
+    for (int i = 0; i < 30; ++i) {
+        for (int j = 0; j < 30; ++j) {
+            std::cout << result_scalar_subtract.pokaz(i, j) << " ";
         }
         std::cout << "\n";
     }
